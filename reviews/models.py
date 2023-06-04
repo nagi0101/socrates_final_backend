68fab767969f2c75877710aa78e6e16161449686
Jiin Park <demon0315@naver.com>
2023-06-04 18:16:37 +0900

    Add MinMax validator to review rate

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Tag(models.Model):
@@ -9,7 +10,9 @@ class Tag(models.Model):
 class Review(models.Model):
     place_id=models.PositiveIntegerField(primary_key=True, editable=True)
     content=models.TextField()
-    rate=models.FloatField()
+    rate=models.FloatField(
+        validators=[MinValueValidator(1.0), MaxValueValidator(5.0)]
+    )
     tags=models.ManyToManyField(Tag, blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
